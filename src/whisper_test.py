5a5c460c1b052ec4c48838a5c7b52ce893aa30f5
Alperen Hoçur <alperenhocur@gmail.com>
2025-08-12 14:57:38 +0300

    update

--- src/whisper_test.py
+++ src/whisper_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c104f0696f56dee9cea52ceeab1054d6e277d1ef310ab10ed6abc8cbcb36ab08
+oid sha256:65d7f20cfcb90c181d404bf54a0e19be4c1af40f57d61c299c5045a146a4de96
 size 652
