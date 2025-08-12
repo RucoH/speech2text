def31e2ccf0d184ba563bede57c40b8021510bea
Alperen Hoçur <alperenhocur@gmail.com>
2025-08-12 11:17:55 +0300

    new model test

--- src/seamless_m4t_test.py
+++ src/seamless_m4t_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee49e79c1bb75c958cb0b531f1f7639b887eb80205fd67d78292ab0cec3cbc6b
-size 2147
+oid sha256:216caeab26cd68f949df10a2bbec5bc1ebbd5b079bdf66146caf329f38e921d3
+size 2163
