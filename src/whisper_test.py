ba9df38d055aedce2a6f929a9e7f15785a22acca
Alperen Hoçur <alperenhocur@gmail.com>
2025-08-13 15:31:42 +0300

    new day new models

--- src/whisper_test.py
+++ src/whisper_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65d7f20cfcb90c181d404bf54a0e19be4c1af40f57d61c299c5045a146a4de96
-size 652
+oid sha256:7713b02e578e86d1c23272998ad4ed9c4afa10e9525f1ee18be251dfe158295d
+size 1282
