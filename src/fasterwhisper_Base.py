5b79995192119d7f42a0d26e68241d2132da7e5b
Alperen Hoçur <alperenhocur@gmail.com>
2025-08-18 16:34:20 +0300

    CPU tests

---
+++ src/fasterwhisper_Base.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:15228fe427ee11a683bb247e083db241772243f44d50fd7dc5e83cf12054e9b4
+size 1728
---
+++ src/fasterwhisper_large_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f4e42e19dc8fc2cc6ecc6d7467842060d3f68cc5dba8bf6d085fac9004ee74d
+size 6497
