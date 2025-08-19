53e12fc1c27f5ceae77ddf34de3a12da313234b4
Alperen Hoçur <alperenhocur@gmail.com>
2025-08-19 13:38:23 +0300

    Delete chunks

--- src/fasterwhisper_large_test.py
+++ src/fasterwhisper_large_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f4e42e19dc8fc2cc6ecc6d7467842060d3f68cc5dba8bf6d085fac9004ee74d
-size 6497
+oid sha256:9caea7bb69c5e60d6c99c083fef0a015ade7fed524474c7a0dcd722ea69ab1d4
+size 13420
---
+++ src/fasterwhisper_small_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c3cb79d84451d36b889e9d04405d1867ef97d8fb697e0b7af0f130e401b5683
+size 9201
